92dea06a36b825c6da21c80ac95c2f606b71143d
Nathan-dev-dot <maerna1@gmail.com>
2024-04-12 18:13:37 +0200

    add some logic in task and todolist

--- src/main/java/org/tododone/todolist/Task.java
+++ src/main/java/org/tododone/todolist/Task.java
@@ -1,4 +1,15 @@
 package org.tododone.todolist;
 
+import java.time.LocalDateTime;
+
 public class Task {
+    LocalDateTime creation_date;
+    String content;
+    Boolean done;
+
+    public Task(LocalDateTime creation_date, String content, Boolean done) {
+        this.creation_date = creation_date;
+        this.content = content;
+        this.done = done;
+    }
 }
